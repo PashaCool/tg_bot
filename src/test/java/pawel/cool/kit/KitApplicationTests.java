988854a444eda9a1a615b0f15eba4188ee3318cc
p.yakovlev <pavel.yakovlev@artezio.com>
2020-04-28 14:18:27 +0300

    init kit-bot

---
+++ src/test/java/pawel/cool/kit/KitApplicationTests.java
@@ -0,0 +1,13 @@
+package pawel.cool.kit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
